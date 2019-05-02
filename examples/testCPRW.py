0efc0f6203b9d5a97ed3b200e74b8e616e7f4567
David Hohn <david.hohn@cern.ch>
2019-05-02 14:50:31 +0200

    update python example code

--- examples/testCPRW.py
+++ examples/testCPRW.py
@@ -1,8 +1,7 @@
 #! /bin/env python2
 
 def main(useEventStore,silent,useNewEvent,iterations):
-    from HLeptonsCPRW import OptObsEventStore
-    import ROOT
+    import HLeptonsCPRW
     import array
     pjet1 = array.array('d', [438.019730, -24.873165, -94.306022, 427.023386])               # E,px,py,pz of nth final state parton
     pjet2  = array.array('d',[656.475632, -55.150478, 66.466227, -650.769506])
@@ -22,10 +21,10 @@ def main(useEventStore,silent,useNewEvent,iterations):
     eventNumber = 1234;
 
     # Only used when ran without the Event-store:
-    pdf1 = array.array('d',[0,0.0391232,0.0541232,0.0845228,0.105186,0.129429,0.86471,0.345418,0.561297,0.0845228,0.0541232,0.0391232,0])  #from -6 to 6: pdfs for 1st parton
-    pdf2 = array.array('d',[0,0.0143834,0.0205766,0.0334123,0.0462144,0.0601489,0.345621,0.246406,0.468401,0.0334123,0.0205766,0.0143834,0])  #from -6 to 6: pdfs for 2nd parton
+    pdf1 = array.array('d',[0, 0.0391232, 0.0541232, 0.0845228, 0.105186,  0.129429,  0.86471,  0.345418, 0.561297, 0.0845228, 0.0541232, 0.0391232, 0])  #from -6 to 6: pdfs for 1st parton
+    pdf2 = array.array('d',[0, 0.0143834, 0.0205766, 0.0334123, 0.0462144, 0.0601489, 0.345621, 0.246406, 0.468401, 0.0334123, 0.0205766, 0.0143834, 0])  #from -6 to 6: pdfs for 2nd parton
 
-    ooES = OptObsEventStore();
+    ooES = HLeptonsCPRW.OptObsEventStore();
     if (useEventStore):
       if (silent == False):
         print("Using EventStore")
@@ -63,16 +62,16 @@ def main(useEventStore,silent,useNewEvent,iterations):
 
       else:
 
-        weights = ROOT.HLeptonsCPRW.getWeightsDtilde(ecm, mH , npafin,flavour1In,flavour2In,flavour1Out,flavour2Out,flavour3Out,x1,x2,pjet1,pjet2,pjet3,phiggs);
+        weights = HLeptonsCPRW.getWeightsDtilde(ecm, mH , npafin,flavour1In,flavour2In,flavour1Out,flavour2Out,flavour3Out,x1,x2,pjet1,pjet2,pjet3,phiggs);
         if not silent:
           print("Calling getWeightsDtilde(...)! Results are: {} , {}".format(weights.first,weights.second))
 
-        oo = ROOT.HLeptonsCPRW.getOptObs(ecm, mH ,x1,x2,pdf1,pdf2,pjet1,pjet2,phiggs);
+        oo = HLeptonsCPRW.getOptObs(ecm, mH ,x1,x2,pdf1,pdf2,pjet1,pjet2,phiggs);
         if not silent:
           print("Calling getOptObs(...)! Results are: {} , {}".format(oo.first,oo.second))
 
         rw = \
-        ROOT.HLeptonsCPRW.getReweight(ecm, mH, 1 , \
+        HLeptonsCPRW.getReweight(ecm, mH, 1 , \
         0.5, 0.5, 0.5, 0.5, 0.5, #rsmin,din,dbin,dtin,dtbin \
         0.5, 0.5, 0.5,           #a1hwwin,a2hwwin,a3hwwin \
         0.5, 0.5, 0.5,           #a1haain,a2haain,a3haain \
@@ -116,4 +115,4 @@ if __name__ == '__main__':
       #if (TString(argv[i]).Contains("--useNewEvent")) useNewEvent = true;
       #if (TString(argv[i]) == "-i") iterations = atoi(argv[i+1]);
     #}
-    main(args.useEventStore,args.silent,args.useNewEvent,args.iterations)
+    main(args.useEventStore,args.silent,args.useNewEvent,int(args.iterations))
