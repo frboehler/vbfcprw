829160505eb052cd97e5ea20e2b98919f455086a
David Hohn <david.hohn@cern.ch>
2019-05-02 14:49:49 +0200

    create root python dictionaries

---
+++ HLeptonsCPRW/HLeptonsCPRWDict.h
@@ -0,0 +1,7 @@
+// -*- c++ -*- file
+#ifndef __HLeptonsCPRWDICT__
+#define __HLeptonsCPRWDICT__
+#include "HLeptonsCPRW/OptObsEventStore.h"
+#include "HLeptonsCPRW/WeightAndOptObsWrapper.h"
+
+#endif //__HLeptonsCPRWDICT__
---
+++ python/__init__.py
@@ -0,0 +1,9 @@
+# make class and functions available in this namespace
+
+#class
+from ROOT import OptObsEventStore
+
+#functions
+from ROOT.HLeptonsCPRW import getOptObs
+from ROOT.HLeptonsCPRW import getReweight
+from ROOT.HLeptonsCPRW import getWeightsDtilde
